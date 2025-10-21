48d3b05fa6c64338d4460442c58a815bebc27877
Chief <122539082+Chief-Github@users.noreply.github.com>
2025-10-21 16:08:59 +0100

    changing wallpaper name + adding wallpapers into files

--- install.sh
+++ install.sh
@@ -115,6 +115,7 @@ apps=(
   fastfetch
   thunar
   swayosd
+  code
   conky
   rofi
   wofi
@@ -260,6 +261,10 @@ case "$choice" in
   *) theme="purple" ;;
 esac
 
+mkdir -p ~/wallpaper
+cp -a -- "${SCRIPT_DIR}/wallpaper"/. "~/wallpaper"/
+
+"${SCRIPT_DIR}/config"
 # ---------------- Theme symlinks ----------------
 ln -sf ~/.config/conky/themes/conky_$theme.conf ~/.config/conky/conky.conf
 ln -sf ~/.config/kitty/themes/kitty_$theme.conf ~/.config/kitty/kitty.conf
